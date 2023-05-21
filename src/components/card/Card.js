4c6b7903956f3f46ab980ded8c83d68a0c25209e
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2023-05-21 20:07:36 +0530

    removed bugs

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,44 +1,48 @@
-import React ,{ useState , useEffect}from 'react'
-import { Link } from 'react-router-dom'
-import Skeleton ,{SkeletonTheme} from 'react-loading-skeleton'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import './Card.css'
+import "./Card.css";
 
-const Card = ({movie}) => {
-    const [isLoading,setIsLoading] = useState(true)
+const Card = ({ movie }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>(
-        setTimeout(()=>{
-            setIsLoading(false)
-        },2000)
-    ),[])
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-     {
-        isLoading
-        ?
+      {isLoading ? (
         <div className="cards">
-            <SkeletonTheme color="#202020" highlightColor="#444">
-                <Skeleton height={300} duration={2} />
-            </SkeletonTheme>
+          <SkeletonTheme color="#202020" highlightColor="#444">
+            <Skeleton height={300} duration={2} />
+          </SkeletonTheme>
         </div>
-        :
-        <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
-            <div className="cards">
-                <img className="card_img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
-                <div className="card_overlay">
-                    <div className="card_title">{movie?movie.original_title:""}</div>
-                    <div className="card_runtime">
-                        {movie?movie.release_date:""}
-                        <span className="card_rating">{movie?movie.vote_average:""}<i className="fas fa-star" /></span>
-                    </div>
-                    <div className="card_description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div>
-                </div>
+      ) : (
+        <Link to={`/movie/${movie.id}`}  style={{ textDecoration: "none", color: "white" }}>
+          <div className="cards">
+            <img className="card_img" src={`https://image.tmdb.org/t/p/original${ movie.poster_path || ""}`}alt="poster"/>
+            <div className="card_overlay">
+              <div className="card_title">{movie.original_title || ""}</div>
+              <div className="card_runtime">
+                {movie.release_date || ""}
+                <span className="card_rating">
+                  {movie.vote_average || ""}
+                  <i className="fas fa-star" />
+                </span>
+              </div>
+              <div className="card_description">
+                {movie ? `${movie.overview.slice(0, 118)}...` : ""}
+              </div>
             </div>
+          </div>
         </Link>
-    }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,37 +1,32 @@
-import React ,{ useState , useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import Card from '../card/Card'
-import './MovieList.css'
+import React, { useEffect, useState, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import Card from "../card/Card";
+import "./MovieList.css";
 
 const MovieList = () => {
-    const [movieList,setMovieList] = useState([])
-    const {type} = useParams()
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
 
-    // useEffect(()=>(
-    //     getData()
-    // ),[])
+  const getData = useCallback(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${type || "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  }, [type]);
 
-    useEffect(()=>(
-        getData()
-    ),[type])
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovieList(data.results))
-    }
   return (
-        <div className="movie_list">
-            <h2 className="list_title">{(type ? type :"POPULAR").toUpperCase()}</h2>
-            <div className="list_cards">
-                {
-                    movieList.map( movie =>(
-                        <Card movie={movie}/>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+    <div className="movie_list">
+      <h2 className="list_title">{(type || "POPULAR").toUpperCase()}</h2>
+      <div className="list_cards">
+        { movieList.map((movie) => (
+           <Card movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default MovieList
+export default MovieList;
